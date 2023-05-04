385109000172bb577dd2d6ce1a8a4def0fd622ae
OAtulA <oraonatula@gmail.com>
2023-05-04 23:20:11 +0530

    Just did anoter pattern

---
+++ Practise/Striver_A2Z_DSA_Sheet/Pattern_13/Solution.java
@@ -0,0 +1,22 @@
+// package Pattern_13;
+
+public class Solution {
+    
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        obj.printTriangle(5);
+    }
+
+    private void printTriangle(int n) {
+        int c=1;
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c++ + " ");                                
+            }
+            System.out.println();
+        }
+    }
+}
