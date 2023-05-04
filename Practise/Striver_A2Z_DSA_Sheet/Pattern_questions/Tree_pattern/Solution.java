161c957c3692a880e3f2188f9ad6d9951457ea14
OAtulA <oraonatula@gmail.com>
2023-05-04 23:13:26 +0530

    Did solve a question.

---
+++ Practise/Striver_A2Z_DSA_Sheet/Pattern_questions/Tree_pattern/Solution.java
@@ -0,0 +1,33 @@
+// package Practise.Striver_A2Z_DSA_Sheet.Pattern_questions.Tree_pattern;
+
+import java.io.*;
+
+class Solution{
+
+    void printTriangle(int n) {
+        
+        int c =1;
+        
+        for (int i =0; i<n; i++ ) {
+            for (int j=n-i -1; j>0; j--) System.out.print(" ");
+            
+            for (int k=1; k<=c ; k++)  System.out.print("*");
+            
+            System.out.println();
+            
+            c+=2; 
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+         obj.printTriangle(5);
+        System.out.println();
+        String s =  "     *";
+        System.out.println("Output lenght: "+ s.length() );
+        System.out.println();
+        System.out.println("Expected length: "+ "    *".length() );
+    }
+}
